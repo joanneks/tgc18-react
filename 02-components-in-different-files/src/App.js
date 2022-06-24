ee80c9f11e20f998e7c09735589d5b8440f9d7ab
Paul <chorkunxin@gmail.com>
2022-06-24 16:25:33 +0800

    Added example of exporting three functions

--- 02-components-in-different-files/src/App.js
+++ 02-components-in-different-files/src/App.js
@@ -1,21 +1,7 @@
 import React from 'react';
 import BorderedImage from './BorderedImage';
 import Alert from './Alert';
-
-function sayHello() {
-  return "Hello"
-}
-
-function sayGoodbye() {
-  // Under the hood JSX are just JavaScript objects 
-  // therefore they can be assigned to variables;
-  let g = <p>Goodbye World</p>;
-  return g;
-}
-
-function foobar() {
-  return <h3>Foobar!</h3>
-}
+import {sayHello, sayGoodbye, foobar} from './funcs';
 
 
 
