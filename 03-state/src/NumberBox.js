2b16a7bc0cb8ed0fe88943f7bcba04523652f5b8
Paul <chorkunxin@gmail.com>
2022-06-27 11:26:32 +0800

    Added NumberBox demo

--- 03-state/src/NumberBox.js
+++ 03-state/src/NumberBox.js
@@ -3,6 +3,29 @@ import React from 'react'
 
 // A class is a template for an object
 // `extends` means inherit -- the NumberBox class has all the methods and variables from the React.Component class
-class NumberBox extends React.Component {
-
+export default class NumberBox extends React.Component {
+   
+    // `state` is an object
+    // that contains key/value pairs 
+    // intended for data that is accessible by the
+    // component itself. It's private data so no
+    // other components can access it or change it
+    state = {
+        "count":10
+    }
+   
+    render() {
+        return (
+            <div style={
+                {
+                    'border':'1px solid black',
+                    'padding':'10px',
+                    'width': '20px'
+                }
+            }>{this.state.count}</div>
+        )
+    }
 }
+
+// alternative method of exporting:
+// export default NumberBox
