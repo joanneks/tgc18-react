2b8d5e3ffdfb02d02d97e37837b8f3ba805d754e
Paul <chorkunxin@gmail.com>
2022-06-30 14:15:36 +0800

    Added solution for edit

--- 08-hands-on-1/src/App.js
+++ 08-hands-on-1/src/App.js
@@ -21,12 +21,15 @@ export default class App extends React.Component {
       }
     ],
     newUserName: "",
-    newUserEmail: ""
+    newUserEmail: "",
+    userBeingEdited: null,
+    modifedUserName:"",
+    modifiedUserEmail:""
   };
 
   updateFormField = (event) => {
     this.setState({
-      [event.target.name]:event.target.value
+      [event.target.name]: event.target.value
     })
   }
 
@@ -52,6 +55,79 @@ export default class App extends React.Component {
     );
   }
 
+  displayUser = (user) => {
+    return (
+      <React.Fragment>
+        <h3>{user.name}</h3>
+        <h4>{user.email}</h4>
+        <button
+          onClick={() => {
+            this.beginEdit(user);
+          }}
+        >
+          Update
+        </button>
+        <button
+          onClick={() => {
+            this.deleteUser(user);
+          }}
+        >
+          Delete
+        </button>
+      </React.Fragment>
+    )
+  }
+
+  displayEditUser = (user) => {
+    return (
+      <React.Fragment>
+        <div>
+          <input type="text" name="modifiedUserName" value={this.state.modifiedUserName} onChange={this.updateFormField}/>
+        </div>
+        <div>
+          <input type="text" name="modifiedUserEmail" value={this.state.modifiedUserEmail} onChange={this.updateFormField}/>
+        </div>
+        <button onClick={this.processUpdateUser}>Confirm</button>
+      </React.Fragment>
+    )
+  }
+
+  processUpdateUser = () => {
+    // clone the object
+    const modifiedUser = {
+      ...this.state.userBeingEdited,
+      name: this.state.modifiedUserName,
+      email: this.state.modifiedUserEmail
+    }
+
+    // replace into the middle of the array
+
+    // 0. find the index of the user that we want to replace
+    let index = -1; // NOT FOUND
+    for (let i =0; i < this.state.users.length; i++) {
+      if (this.state.users[i]._id === modifiedUser._id) {
+        index = i;
+        break;
+      }
+    }
+
+    if (index===-1) {
+      return;
+    }
+
+    // 1. clone the array in the state
+    const cloned = [
+      ...this.state.users.slice(0, index),
+      modifiedUser,
+      ...this.state.users.slice(index+1)
+    ]
+
+    this.setState({
+      users: cloned,
+      userBeingEdited: null // indicate that no user is being edited at the moment 
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -59,22 +135,14 @@ export default class App extends React.Component {
           return (
             <React.Fragment key={user._id}>
               <div class="box">
-                <h3>{user.name}</h3>
-                <h4>{user.email}</h4>
-                <button
-                  onClick={() => {
-                    this.beginEdit(user);
-                  }}
-                >
-                  Update
-                </button>
-                <button
-                  onClick={() => {
-                    this.deleteUser(user);
-                  }}
-                >
-                  Delete
-                </button>
+                 {
+                    this.state.userBeingEdited === null || this.state.userBeingEdited._id !== user._id ?
+                      this.displayUser(user)
+                      :
+                      this.displayEditUser(user)
+                 }
+
+
               </div>
             </React.Fragment>
           );
@@ -98,7 +166,7 @@ export default class App extends React.Component {
 
     // alternatively, do #1 and #2 together
     // let cloned = [...this.state.users, userObject]
-    
+
     // 3. replace the cloned array into the state
     this.setState({
       users: cloned
@@ -114,7 +182,14 @@ export default class App extends React.Component {
     // })
   };
 
-  beginEdit = (user) => {};
+  beginEdit = (user) => {
+    // 1. add in a state variable to remember which user is being edited
+    this.setState({
+      userBeingEdited: user,
+      modifiedUserName: user.name,
+      modifiedUserEmail: user.email
+    })
+  };
 
-  deleteUser = (user) => {};
+  deleteUser = (user) => { };
 }
